b41b17a1824263b6e8f4d2bb90be1d1d9785d320
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-01-06 12:56:16 +0530

    Added - firebase initizlisation configuration for both web and mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // responsive
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -8,12 +11,31 @@ import 'package:instagram_clone/responsive/web_screen_layout.dart';
 // utils
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCQoClO465DmCcnvXxguL6X-GZiMiasqps",
+            appId: "1:834697553646:web:7997468911f3bc394bf5ff",
+            messagingSenderId: "834697553646",
+            projectId: "instagramclone-995a6",
+            storageBucket: "instagramclone-995a6.appspot.com"));
+  } else {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  /* Future<void> initializeDefault() async {
+    FirebaseApp app = await Firebase.initializeApp();
+    print('Initialized default app $app');
+  } */
 
   // This widget is the root of your application.
   @override
